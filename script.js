a5d55d1918b10dc0e811dc358a10bde959a38f62
Bailey <jbailey@budcentral.com>
2021-06-21 15:44:10 -0500

    responsive price

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const slider = document.getElementById('slider-bar');
 const price = document.getElementById('price');
+const mobilePrice = document.getElementById('mobile-price')
 
 //load initial price
-price.textContent = '$' + slider.value;
+loadPrice = () => {
+    if (mobilePrice) {
+        mobilePrice.textContent = '$' + slider.value;
+    } else {
+        price.textContent = '$' + slider.value;
+    }
+}
 
 //update price and slider track based on slider position
 slider.oninput = function() {
-    price.textContent = '$' + this.value;
-    
+    price.textContent = '$' + Math.floor(this.value * .33);
+    mobilePrice.textContent = '$' + Math.floor(this.value * .33);
     let thumbPosition = this.value;
     //const sliderTrack = document.querySelector("input[type='range']::-webkit-slider-runnable-track");
     slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%) ${thumbPosition}% , hsl(224, 65%, 95%)${thumbPosition}%)`;
-
-    return price.textContent;
 }
 
+loadPrice();
 slider.oninput();
